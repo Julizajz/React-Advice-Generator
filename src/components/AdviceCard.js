bd191445cabf64e01b9c73e0d3018f1c2788fa86
Adrian Visnieski <adrianivsdk@gmail.com>
2023-09-14 18:07:57 -0300

    first commit

---
+++ src/components/AdviceCard.js
@@ -0,0 +1,16 @@
+import dividerDesktop from '../images/pattern-divider-desktop.svg'
+import DiceButtton from './DiceButtton'
+
+const AdviceCard = () => {
+  return (
+    <div>
+        <h1>Advice #1</h1>
+        <p>advice</p>
+        <img src ={dividerDesktop} alt='card-divider'/>
+        <DiceButtton/>
+
+    </div>
+  )
+}
+
+export default AdviceCard
---
+++ src/components/DiceButtton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import diceImage from '../images/icon-dice.svg'
+
+const DiceButtton = () => {
+  return (
+    <button>
+        <img src={diceImage} alt='dice'/>
+    </button>
+  )
+}
+
+export default DiceButtton
